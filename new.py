9bf8f9b51577c5998e439e4d838d74a0d2b2d678
reema <reem.dandachi@gmail.com>
2025-01-14 16:36:19 +0100

    part 3!

--- new.py
+++ new.py
@@ -1,80 +1,59 @@
 import json
 import csv
 res = {}
-
-contents = [
-    'Seatle',
-    'Cincinnati',
-    'Maui',
-    'San Diego'
-]
-
-#loop over all stations
-for city_name in contents:  
-    with open ('precipitation.json', 'r') as file:
+with open ('stations.csv',) as file: 
+    stations = list(csv.DictReader(file))
+with open ('precipitation.json', 'r') as file:
         contents = json.load(file)
 
+#finding all the data for the various statiosn
+for station in stations: 
+    #city_date
+    station_number = station['Station']
+    state = station['State']
+    city_name = station['Location']
 
-# Look into the all the various data' s
-seatle_station = 'GHCND:US1WAKG0038' #Ensure that this is the correct code
-seatle_data = []
-Cincinnati_station = 'GHCND: USW00093814'
-Cincinnati_data = []
-Maui_station = 'GHCND:USC0051331'
-San_Diego_station= 'GHCND:US1CASD0032'
-Maui_data=  []
-San_Diego_data = []
+    station_data=[]
+    for measurements in contents: 
+        if measurements['station'] == station['Station']: 
+            station_data.append(measurements)
 
-# assign the measurments per city 
-for measurements in contents: 
-    if measurements['station'] == seatle_station: 
-        seatle_data.append(measurements)
-    if measurements['station'] == Cincinnati_station:
-        Cincinnati_data.append(measurements)
-    if measurements['station'] == Maui_station:
-        Maui_data = []
-    if measurements['station'] == San_Diego_station:
-        San_Diego_data.append(measurements)
+    #Seatle monthly precpitation: a list with the total precipitation per month
+
+    total_month_precipitation = {} # create a dictionary 
+
+    for measurements in station_data: 
+        month =  int(measurements['date'].split("-")[1]) #splitting the data to only get the months, which is in the second column, hence we are using 1
+        if month in total_month_precipitation: #trying to get the sum of the seperate months
+            total_month_precipitation[month] +=  measurements['value']
+        else:
+            total_month_precipitation[month] = 0
         
+    print(total_month_precipitation)
 
-
-
-#Seatle monthly precpitation: a list with the total precipitation per month
-
-total_month_precipitation = {} # create a dictionary 
-
-for measurements in contents: 
-  month =  int(measurements['date'].split("-")[1]) #splitting the data to only get the months, which is in the second column, hence we are using 1
-  if month in total_month_precipitation: #trying to get the sum of the seperate months
-      total_month_precipitation[month] +=  measurements['value']
-  else:
-      total_month_precipitation[month] = 0
-      
-print(total_month_precipitation)
-
-# Summing the precipitation values for each year
-  
-total_yearly_percipitation = sum(total_month_precipitation.values())
-
-# The relative_monhtly_precipitation
-
-relative_monthly_precipitation= {} 
-# type: ignore
-for month in total_month_precipitation:
-    relative_monthly_precipitation[month] = (total_month_precipitation[month] / total_yearly_percipitation)
+    # Summing the precipitation values for each year
     
-print(relative_monthly_precipitation)
+    total_yearly_percipitation = sum(total_month_precipitation.values())
 
-#create a column of what will show up in the Jason file
-res[city_name]=  {
-    'month_precipitation' : total_month_precipitation,
-    'year_precipitation' : total_yearly_percipitation, 
-    'relative_precipitation' : relative_monthly_precipitation
-            }    
+    # The relative_monhtly_precipitation
+
+    relative_monthly_precipitation= {} 
+    # type: ignore
+    for month in total_month_precipitation:
+        relative_monthly_precipitation[month] = (total_month_precipitation[month] / total_yearly_percipitation)
+        
+    print(relative_monthly_precipitation)
+
+    #create a column of what will show up in the Jason file
+    res[city_name]=  {
+        'month_precipitation' : total_month_precipitation,
+        'year_precipitation' : total_yearly_percipitation, 
+        'relative_precipitation' : relative_monthly_precipitation
+                }    
 
 # Create a Jason file 
 with open('results.json', 'w') as file: 
-    json.dump(res, file, indent = 20)
+    json.dump(res, file, indent = 4)
 
 
 
